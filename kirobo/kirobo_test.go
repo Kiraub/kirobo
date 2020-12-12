b8efa26844555b9ebd64abb0896c743f75337478
Kiraub <kiraub.1997@gmail.com>
2020-12-12 23:21:03 +0100

    logger struct implemented with basic test

--- kirobo/kirobo_test.go
+++ kirobo/kirobo_test.go
@@ -1,1 +1,32 @@
 package kirobo
+
+import (
+	"fmt"
+	"kirobo/logger"
+	"testing"
+)
+
+func TestLogger(t *testing.T) {
+	defaultLogger := logger.CreateLogger()
+	ti := "Test info message"
+	tif := fmt.Sprintf(logger.InfoFormat, ti)
+	td := "Test debug message"
+	tdf := fmt.Sprintf(logger.DebugFormat, td)
+	te := "Test error message"
+	tef := fmt.Sprintf(logger.ErrorFormat, te)
+	if lio, err := defaultLogger.Infof(ti); err != nil {
+		t.Error(err)
+	} else if ltif := len(tif); lio != ltif {
+		t.Errorf("Infof bytes e: %v; f: %v", ltif, lio)
+	}
+	if ldo, err := defaultLogger.Debugf(td); err != nil {
+		t.Error(err)
+	} else if ltdf := len(tdf); ldo != ltdf {
+		t.Errorf("Infof bytes e: %v; f: %v", ltdf, ldo)
+	}
+	if leo, err := defaultLogger.Errorf(te); err != nil {
+		t.Error(err)
+	} else if ltef := len(tef); leo != ltef {
+		t.Errorf("Infof bytes e: %v; f: %v", ltef, leo)
+	}
+}
--- logger/logger.go
+++ logger/logger.go
@@ -6,9 +6,12 @@ import (
 )
 
 const (
-	infoFormat  = "INFO: %v\n"
-	errorFormat = "ERROR: %v\n"
-	debugFormat = "DEBUG: %v\n"
+	// InfoFormat is the default info logging format
+	InfoFormat = "INFO: %v\n"
+	// DebugFormat is the default debug logging format
+	DebugFormat = "DEBUG: %v\n"
+	// ErrorFormat is the default error logging format
+	ErrorFormat = "ERROR: %v\n"
 )
 
 var (
@@ -20,7 +23,7 @@ var (
 	ErrorLogEnabled = true
 )
 
-func fprintf(file *os.File, metaFormat string, format string, args ...interface{}) (int, error) {
+func fPrintf(file *os.File, metaFormat string, format string, args ...interface{}) (int, error) {
 	return fmt.Fprintf(file, fmt.Sprintf(metaFormat, fmt.Sprintf(format, args...)))
 }
 
@@ -29,7 +32,7 @@ func Infof(format string, args ...interface{}) (int, error) {
 	if !InfoLogEnabled {
 		return 0, nil
 	}
-	return fprintf(os.Stdout, infoFormat, format, args...)
+	return fPrintf(os.Stdout, InfoFormat, format, args...)
 }
 
 // Debugf prints debug log to the default out stream. It returns bytes written and any error encountered.
@@ -37,7 +40,7 @@ func Debugf(format string, args ...interface{}) (int, error) {
 	if !DebugLogEnabled {
 		return 0, nil
 	}
-	return fprintf(os.Stderr, debugFormat, format, args...)
+	return fPrintf(os.Stderr, DebugFormat, format, args...)
 }
 
 // Errorf prints error log to the default out stream. It returns bytes written and any error encountered.
@@ -45,5 +48,42 @@ func Errorf(format string, args ...interface{}) (int, error) {
 	if !ErrorLogEnabled {
 		return 0, nil
 	}
-	return fprintf(os.Stderr, errorFormat, format, args...)
+	return fPrintf(os.Stderr, ErrorFormat, format, args...)
+}
+
+// Logger is a struct that contains logging functions
+type Logger struct {
+	InfoFormat  string
+	DebugFormat string
+	ErrorFormat string
+	InfoOut     *os.File
+	DebugOut    *os.File
+	ErrorOut    *os.File
+}
+
+// CreateLogger returns a new logger struct with default formatting strings and output streams
+func CreateLogger() *Logger {
+	l := new(Logger)
+	l.InfoFormat = InfoFormat
+	l.DebugFormat = DebugFormat
+	l.ErrorFormat = ErrorFormat
+	l.InfoOut = os.Stdout
+	l.DebugOut = os.Stderr
+	l.ErrorOut = os.Stderr
+	return l
+}
+
+// Infof ...
+func (r *Logger) Infof(format string, args ...interface{}) (int, error) {
+	return fPrintf(r.InfoOut, r.InfoFormat, format, args...)
+}
+
+// Debugf ...
+func (r *Logger) Debugf(format string, args ...interface{}) (int, error) {
+	return fPrintf(r.DebugOut, r.DebugFormat, format, args...)
+}
+
+// Errorf ...
+func (r *Logger) Errorf(format string, args ...interface{}) (int, error) {
+	return fPrintf(r.ErrorOut, r.ErrorFormat, format, args...)
 }
